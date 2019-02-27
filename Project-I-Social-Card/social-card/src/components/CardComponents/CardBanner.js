cbc0c6a13b538d3a957ed1d527081b590c15d3e4
Mychal Hall <hallmychal@gmail.com>
2019-02-27 17:07:16 -0600

    MVP complete

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+function CardBanner() {
+    return (
+        <div className="cardBanner">
+        <img className="reactPicture" src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"></img>
+        </div>
+    );
+}
+
+export default CardBanner;
