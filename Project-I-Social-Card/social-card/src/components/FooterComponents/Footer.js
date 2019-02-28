4075e0fc92306fb5ff901057b4b6bca5dc996d6a
Mychal Hall <hallmychal@gmail.com>
2019-02-27 21:48:21 -0600

    Functionality build for icons 50%

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "./Footer.css";
 
+
+function commentClicked(){
+    console.log('you clicked me');
+}
+
+function shareClicked(){
+    console.log('you clicked me');
+}
+
+function heartClicked(){
+    console.log('you clicked me');
+}
+
+function mailClicked(){
+    console.log('you clicked me');
+}
+
 function Footer() {
   return (
     <div className="footerBar">
-      <i onClick="" class="far fa-comment-alt" />
-      <i class="fas fa-sync-alt" />
-      <i class="far fa-heart" />
-      <i class="far fa-envelope" />
+      <i onClick={commentClicked} className="far fa-comment-alt" />
+      <i onClick={shareClicked} className="fas fa-sync-alt" />
+      <i onClick={heartClicked} className="far fa-heart" />
+      <i onClick={mailClicked} className="far fa-envelope" />
     </div>
   );
 }
